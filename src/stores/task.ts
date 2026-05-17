aa2315374c3469892c84a1dfed516a256ed2f5d6
nambelaas <salmaniseif@gmail.com>
2026-05-17 19:56:40 +0700

    add setup store

--- src/stores/task.ts
+++ src/stores/task.ts
@@ -1,26 +1,32 @@
 import { allTasks } from "@/http/task-api";
 import { defineStore } from "pinia";
+import { computed, reactive, ref } from "vue";
 
 
-export const useTaskStore = defineStore('taskStore', {
+const tmp = {
     state: () => ({
-        tasks: <Object[]>([]),
-        task: {
-            id: 1,
-            name: 'First Task',
-            is_completed: true
-        }
+
     }),
     getters: {
-        completedTasks: (state) => state.tasks.filter(task => task.is_completed),
-        uncompletedTasks(state) {
-            return state.tasks.filter(task => !task.is_completed)
-        }
     },
     actions: {
-        async fetchAllTasks() {
-            const { data } = await allTasks()
-            this.tasks = data.data
-        }
+
+    }
+}
+
+export const useTaskStore = defineStore('taskStore', () => {
+    const tasks = ref([])
+
+    const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
+
+    const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
+
+    const fetchAllTasks = async () => {
+        const { data } = await allTasks()
+        tasks.value = data.data
+    }
+
+    return {
+        tasks, completedTasks, uncompletedTasks, fetchAllTasks
     }
 })
