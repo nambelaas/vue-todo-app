a932d98290dbbb494643f3a3c667620353fb905a
nambelaas <salmaniseif@gmail.com>
2026-05-18 17:35:56 +0700

    add navigation guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
-export default createRouter({
+const router = createRouter({
     routes,
     history: createWebHistory(),
     linkActiveClass: 'font-semibold text-green-600 hover:text-green-500'
 })
+
+router.beforeEach((to, from) => {
+    console.log('Global before each', to, from)
+
+    if (to.path === '/tasks' || to.path === '/summary') {
+        return { name: 'Login' }
+    }
+
+})
+
+export default router
